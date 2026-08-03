2f9f2d341e659de0f8e916ef0653e251db5e0cfd
Nik-ita2005 <nikitagdg27@gmail.com>
2026-08-03 16:57:11 +0530

    Fix client gitignore, make API URL configurable via env variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
-// Automatically attach the token to every request, if it exists
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
